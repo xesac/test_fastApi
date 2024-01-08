e3f26936547264d3c681bb4362cb644935aa0a93
Maksim <knyazev.maksim2852@gmail.com>
2024-01-08 04:01:05 +0400

    new v

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('hello')
+from fastapi import FastAPI, Path
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+class User(BaseModel):
+    
+    name: str
+    age: int
+    email: str
+    password: str
+
+
+@app.post('/create_user')
+def create_user(user_model: User):
+    return 'a'
+
