be5db897fdc79c0664cd58ba699916f8a754297b
Maksim <knyazev.maksim2852@gmail.com>
2024-01-09 17:21:10 +0400

    more func

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from fastapi import FastAPI, Path
-from fastapi.responses import FileResponse
-from pydantic import BaseModel
-
-
+from fastapi import FastAPI, Path, Query, Body
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-class User(BaseModel):
-    
-    name: str
-    age: int
-    email: str
-    password: str
+
+@app.get("/")
+def root():
+    return FileResponse("public/index.html")
 
 
-@app.post('/create_user')
-def create_user(user_model: User):
-    return 'a'
-
+@app.post("/hello")
+def hello(name:str  = Body(embed=True, min_length=3, max_length=20), 
+            age: int = Body(embed=True, ge=18, lt=111)):
+    return {"message": f"{name}, ваш возраст - {age}"}
